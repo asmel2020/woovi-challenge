8ed3bb3d524f2d5cc01b8628a328938aa8f20d0d
danny gonzalez <jesusgalicia2019@gmail.com>
2024-07-11 21:19:40 -0400

    update navbar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,16 +1,16 @@
-import Image from 'next/image'
-import React from 'react'
-import { LogoNavbarIco } from '../Svg'
-import Link from 'next/link'
+import Image from "next/image";
+import React from "react";
+import { LogoNavbarIco } from "../Svg";
+import Link from "next/link";
 
 export const NavBar = () => {
   return (
     <nav className="flex justify-center w-full h-14 mt-9">
-    <Link href={"/"}>
-    <figure className="">
-     <LogoNavbarIco />
-    </figure>
-    </Link>
-  </nav>
-  )
-}
+      <Link href={"/"}>
+        <figure className="">
+          <LogoNavbarIco />
+        </figure>
+      </Link>
+    </nav>
+  );
+};
